174948c4aa43a896b2ab45ba499b18c34af7d87c
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-08-22 15:05:18 -0500

    finishes app

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -36,5 +36,23 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         }
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsVC {
+            let barBtn = UIBarButtonItem()
+            barBtn.title = " "
+            navigationItem.backBarButtonItem = barBtn
+            
+            // assert is a layer of protection
+            assert(sender as? Category != nil)
+            productsVC.initProduct(category: sender as! Category)
+
+        }
+    }
+    
 }
 
