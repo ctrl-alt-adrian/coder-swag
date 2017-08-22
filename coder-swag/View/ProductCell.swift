eb22c67a76f90427505039849eaf12cb77927341
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-08-22 12:26:34 -0500

    adds collection vell cell

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Adrian Thomas on 8/22/17.
+//  Copyright © 2017 Adrian Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
